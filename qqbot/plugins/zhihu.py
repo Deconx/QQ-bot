bcb6798ba6f970b62fb05d82caf0ce1e8b57ad69
Deconx <deconx@vip.qq.com>
2022-05-18 19:04:10 +0800

    add new posts to zhihu

--- qqbot/plugins/zhihu.py
+++ qqbot/plugins/zhihu.py
@@ -8,8 +8,10 @@ import time
 import requests
 
 
-zhihuColumn = "c_1480603406519238656"
-startURL = "https://www.zhihu.com/api/v4/columns/" + zhihuColumn + "/items"
+zhihuColumn1 = "c_1480603406519238656"
+zhihuColumn2 = "c_1502374640542023680"
+URL1 = "https://www.zhihu.com/api/v4/columns/" + zhihuColumn1 + "/items"
+URL2 = "https://www.zhihu.com/api/v4/columns/" + zhihuColumn2 + "/items"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"}
 
 
@@ -27,22 +29,40 @@ def getJSON(url):
 
 
 # 输入文章总数,输出所有文章标题和链接的CSV文件
-def process(total):
+def process(total, url):
     num = 0  # 文章编号
     posts = []
     for offset in range(0, total, 10):
-        jsonData = getJSON(startURL.format(offset))
+        jsonData = getJSON(url.format(offset))
         items = jsonData["data"]
         for item in items:
             num = num + 1
             posts.append([num, item["title"], item["url"]])
     return posts
+jsonData1 = getJSON(URL1.format(0))
+posts1 = process(jsonData1["paging"]["totals"], URL1)
 
-zhihu = on_keyword(['知乎', '文章'], priority=20)
+jsonData2 = getJSON(URL2.format(0))
+posts2 = process(jsonData2["paging"]["totals"], URL2)
+
+posts = posts1 + posts2
+
+mes = []
+index = random.randint(0, 10)
+for i in range(0, len(posts)):
+    mes += [f'第{str(i)}篇：{posts[i][1]}\n{posts[i][2]}']
+print(mes)
+
+zhihu = on_keyword(['知乎', 'cmu', 'csapp', 'mit', '操作系统', '考研', '博客'], priority=20)
 @zhihu.handle()
 async def zhihu_handle(bot: Bot, event: Event):
-    jsonData = getJSON(startURL.format(0))
-    posts = process(jsonData["paging"]["totals"])
+    jsonData1 = getJSON(URL1.format(0))
+    posts1 = process(jsonData1["paging"]["totals"], URL1)
+
+    jsonData2 = getJSON(URL2.format(0))
+    posts2 = process(jsonData2["paging"]["totals"], URL2)
+
+    posts = posts1 + posts2
     mes = []
     index = random.randint(0, 10)
     for i in range(0, len(posts)):
